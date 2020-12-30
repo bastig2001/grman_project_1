acd3e8f46985390a0b2bccfc96dba9ff774481fe
Sebastian Grman <sebastian.grman@gmail.com>
2020-12-30 16:48:12 +0100

    make sleeptime after each election configurable

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void cycle(Ring&);
+void cycle(Ring&, chrono::milliseconds);
 
 
 int main(int argc, char* argv[]) {
@@ -14,6 +14,7 @@ int main(int argc, char* argv[]) {
 
     size_t number_of_workers{};
     unsigned int number_of_elections{0};
+    chrono::milliseconds after_election_sleeptime{5000};
 
     app.add_option(
         "number-of-workers", 
@@ -23,7 +24,14 @@ int main(int argc, char* argv[]) {
     app.add_option(
         "-n, --number-of-elections",
         number_of_elections,
-        "Number of Elections after which to finish\nDefault is infinit."
+        "Number of Elections after which to finish\n"
+        "Default is infinit."
+    );
+    app.add_option(
+        "--sleep",
+        after_election_sleeptime,
+        "Sleeptime after each Election in milliseconds\n"
+        "Default is a sleeptime of 5 seconds."
     );
 
     CLI11_PARSE(app, argc, argv);
@@ -33,19 +41,19 @@ int main(int argc, char* argv[]) {
     
     if (number_of_elections > 0) {
         for (unsigned int i{0}; i < number_of_elections; i++) {
-            cycle(ring);
+            cycle(ring, after_election_sleeptime);
         }
     }
     else {
         while (true) {
-            cycle(ring);
+            cycle(ring, after_election_sleeptime);
         }
     }
 
     ring.stop();
 }
 
-void cycle(Ring& ring) {
+void cycle(Ring& ring, chrono::milliseconds sleeptime) {
     ring.start_election();
-    this_thread::sleep_for(chrono::seconds(5));
+    this_thread::sleep_for(sleeptime);
 }
