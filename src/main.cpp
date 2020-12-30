ca1c428aab2e4f37e8694dc9542cede3a77e57e8
Sebastian Grman <sebastian.grman@gmail.com>
2020-12-30 16:23:54 +0100

    add CLI and let number of workers be specified

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include "ring.h"
 
+#include "CLI11.hpp"
 #include <thread>
 #include <chrono>
 
 using namespace std;
 
 
-int main() {
-    Ring ring(5);
+int main(int argc, char* argv[]) {
+    CLI::App app("Simulate a Ring with Elections using the Chang and Roberts algorithm");
+
+    size_t number_of_workers{};
+
+    app.add_option(
+        "number-of-workers", 
+        number_of_workers, 
+        "Number of Workers in the Ring"
+    )->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    Ring ring(number_of_workers);
     ring.start();
     ring.start_election();
     this_thread::sleep_for(chrono::seconds(5));
